b280d8f57541c7a9a78be27b60f11cfffe171243
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-10-03 08:00:06 +0200

    Initial commit

---
+++ Restaurants_Webpage/Restaurants_Webpage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Restaurants_Webpage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
