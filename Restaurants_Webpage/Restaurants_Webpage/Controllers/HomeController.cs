18726b277a856decb31dd7799cd18b6fa3a503f1
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-10-04 18:14:48 +0200

    Added utils and also login works well

--- Restaurants_Webpage/Restaurants_Webpage/Controllers/HomeController.cs
+++ Restaurants_Webpage/Restaurants_Webpage/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
 using Restaurants_Webpage.Models;
+using Restaurants_Webpage.Utils;
 using System.Diagnostics;
 
 namespace Restaurants_Webpage.Controllers
@@ -7,6 +12,7 @@ namespace Restaurants_Webpage.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient httpClient = new HttpClient();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -18,9 +24,42 @@ namespace Restaurants_Webpage.Controllers
             return View();
         }
 
+        [Authorize(Roles = UserRolesUtility.Client)]
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Login(string login, string password) 
+        {
+            string loginUrl = "https://localhost:7042/api/Users/login";
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(new HttpMethod("POST"), loginUrl);
+            requestMessage.Content = JsonContent.Create(new { loginOrEmail = login, password = password });
+            HttpResponseMessage response = httpClient.SendAsync(requestMessage).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonData = await response.Content.ReadAsStringAsync();
+                JwtModel? jwt = JsonConvert.DeserializeObject<JwtModel>(jsonData);
+                if (jwt != null)
+                {
+                    HttpContext.Response.Cookies.Append("AccessToken", jwt.AccessToken);
+                    HttpContext.Response.Cookies.Append("RefreshToken", jwt.RefreshToken);
+                    return RedirectToAction("Privacy", "Home");   
+                }
+                else 
+                {
+                    //invalid login/password
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            else 
+            {
+                //handling unknown error
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Restaurants_Webpage/Restaurants_Webpage/Utils/UserRolesUtility.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurants_Webpage.Utils
+{
+    public static class UserRolesUtility
+    {
+        public const string Owner = "OWNER";
+        public const string Supervisor = "SUPERVISOR";
+        public const string OwnerAndSupervisor = $"{Owner},{Supervisor}";
+
+        public const string Employee = "EMPLOYEE";
+        public const string Client = "CLIENT";
+        public const string EmployeeAndClient = $"{Employee},{Client}";
+    }
+}
