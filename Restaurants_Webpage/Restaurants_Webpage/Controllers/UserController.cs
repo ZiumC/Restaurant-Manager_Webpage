82922c1ab44308ebb7764b20d837b36baa4d76ae
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-10-05 12:01:02 +0200

    Added mehanizm that redirects when 401 statuscode appear, also renamed and refactored code

---
+++ Restaurants_Webpage/Restaurants_Webpage/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants_Webpage.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Restaurants_Webpage/Restaurants_Webpage/Utils/HttpRequestUtility.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http;
+
+namespace Restaurants_Webpage.Utils
+{
+    public enum HttpMethods { GET, POST, PUT, DELETE }
+    public static class HttpRequestUtility
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public static async Task<HttpResponseMessage> SendRequestAsync(string url, HttpMethods method, JsonContent jsonBody)
+        {
+            string methodType = "";
+            switch (method)
+            {
+                case HttpMethods.GET:
+                    if (jsonBody != null)
+                    {
+                        throw new Exception("Method GET must had empty body");
+                    }
+                    methodType = "GET";
+                    break;
+
+                case HttpMethods.POST:
+                    methodType = "POST";
+                    break;
+
+                case HttpMethods.PUT:
+                    methodType = "PUT";
+                    break;
+
+                case HttpMethods.DELETE:
+                    if (jsonBody != null)
+                    {
+                        throw new Exception("Method DELETE must had empty body");
+                    }
+                    methodType = "DELETE";
+                    break;
+
+                default:
+                    throw new Exception("Unspecified method");
+            }
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(new HttpMethod(methodType), url);
+            if (jsonBody != null)
+            {
+                requestMessage.Content = jsonBody;
+            }
+
+            return await httpClient.SendAsync(requestMessage);
+        }
+    }
+}
