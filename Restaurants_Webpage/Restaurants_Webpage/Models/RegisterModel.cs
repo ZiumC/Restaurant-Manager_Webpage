84934f2602120f95326baaa6e0eb5984d8309b09
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-10-07 18:53:50 +0200

    added validation on server side also js code has been added too

---
+++ Restaurants_Webpage/Restaurants_Webpage/Models/RegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants_Webpage.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string login { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string email { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string password1 { get; set; }
+        public string password2 { get; set; }
+        public string registerMeAsEmployee { get; set; }
+        public string pesel { get; set; }
+        public DateTime hiredDate { get; set; }
+    }
+}
--- Restaurants_Webpage/Restaurants_Webpage/wwwroot/js/register.js
+++ Restaurants_Webpage/Restaurants_Webpage/wwwroot/js/register.js
@@ -1,1 +1,32 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', function () {
+    document
+        .getElementById('registerMeAsEmployee')
+        .addEventListener('change', empOptionsFields);
+
+})
+
+function empOptionsFields(e) {
+
+    const pesel = document.getElementById("pesel");
+    const hiredDate = document.getElementById("hiredDate");
+
+    if (this.checked) {
+        removeAttr(pesel, 'disabled');
+        removeAttr(hiredDate, 'disabled');
+        addAttr(pesel, 'required');
+        addAttr(hiredDate, 'required');
+    } else {
+        removeAttr(pesel, 'required');
+        removeAttr(hiredDate, 'required');
+        addAttr(pesel, 'disabled');
+        addAttr(hiredDate, 'disabled');
+    }
+}
+
+function removeAttr(element, value) {
+    element.removeAttribute(value)
+}
+
+function addAttr(element, value) {
+    element.setAttribute(value, true);
+}
