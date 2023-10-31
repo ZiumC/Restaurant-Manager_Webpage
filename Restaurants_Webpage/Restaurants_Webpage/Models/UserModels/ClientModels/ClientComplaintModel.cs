8895e0bcb108ef04385f095124b96ad9dbacec19
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-10-31 18:14:40 +0100

    added card with details about client reservation

---
+++ Restaurants_Webpage/Restaurants_Webpage/Models/UserModels/ClientModels/ClientComplaintModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurants_Webpage.Models.UserModels.ClientModels
+{
+    public class ClientComplaintModel
+    {
+        public int IdComplaint { get; set; }
+        public DateTime ComplaintDate { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Restaurants_Webpage/Restaurants_Webpage/Models/UserModels/ClientModels/ClientReservationModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants_Webpage.Models.UserModels.ClientModels
+{
+    public class ClientReservationModel
+    {
+        public int IdReservation { get; set; }
+
+        public DateTime ReservationDate { get; set; }
+        public string Status { get; set; }
+        public int? ReservationGrade { get; set; }
+        public int HowManyPeoples { get; set; }
+        public  ClientComplaintModel? ReservationComplaint { get; set; }
+    }
+}
