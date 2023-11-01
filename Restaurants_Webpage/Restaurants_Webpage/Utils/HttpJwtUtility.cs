89ef1496a47aa930fa1c9a342e3c0d634fb4d481
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-11-01 13:50:13 +0100

    code has been refactored

---
+++ Restaurants_Webpage/Restaurants_Webpage/Utils/HttpJwtUtility.cs
@@ -0,0 +1,47 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Restaurants_Webpage.Utils
+{
+    public class HttpJwtUtility
+    {
+        private readonly string? _jwtCookie;
+        private readonly IConfiguration _config;
+
+        public HttpJwtUtility(IConfiguration config, HttpContext httpContext)
+        {
+            _config = config;
+
+            string jwtCookieName = _config["ApplicationSettings:JwtSettings:CookieSettings:CookieName"];
+
+            try
+            {
+                if (string.IsNullOrEmpty(jwtCookieName))
+                {
+                    throw new Exception("Jwt cookie namne can't be empty");
+                }
+
+                _jwtCookie = httpContext.Request.Cookies[jwtCookieName];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public string? GetJwtCookie() 
+        {
+            return _jwtCookie;
+        }
+
+        public string? GetJwtCookieValue(string fieldName)
+        {
+            if (_jwtCookie != null)
+            {
+                var tokenContent = new JwtSecurityTokenHandler().ReadToken(_jwtCookie) as JwtSecurityToken;
+                return tokenContent?.Claims.First(claim => claim.Type == fieldName).Value;
+            }
+
+            return null;
+        }
+    }
+}
