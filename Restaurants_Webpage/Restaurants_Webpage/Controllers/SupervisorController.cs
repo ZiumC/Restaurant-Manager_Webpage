08ff838e7dfb320f68a3aef6ff46254f22673665
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-11-02 19:01:22 +0100

    added employeeform

--- Restaurants_Webpage/Restaurants_Webpage/Controllers/SupervisorController.cs
+++ Restaurants_Webpage/Restaurants_Webpage/Controllers/SupervisorController.cs
@@ -10,21 +10,30 @@ namespace Restaurants_Webpage.Controllers
     {
         private readonly IConfiguration _config;
         private readonly string _employeesUrl;
+        private readonly string _employeeDetailsUrl;
+
 
         public SupervisorController(IConfiguration config)
         {
             _config = config;
 
             string employeesBaseUrl = string.Concat(_config["Endpoints:BaseHost"], _config["Endpoints:Controller:Employees"]);
+            string employeeDetailsUrl = employeesBaseUrl + "/{0}";
 
             try
             {
                 if (string.IsNullOrEmpty(employeesBaseUrl))
                 {
-                    throw new Exception("mployees base url can't be empty");
+                    throw new Exception("Employees base url can't be empty");
+                }
+
+                if (string.IsNullOrEmpty(employeeDetailsUrl))
+                {
+                    throw new Exception("Employee details url can't be empty");
                 }
 
                 _employeesUrl = employeesBaseUrl;
+                _employeeDetailsUrl = employeeDetailsUrl;
 
             }
             catch (Exception ex)
@@ -56,6 +65,33 @@ namespace Restaurants_Webpage.Controllers
 
             return View(employees);
 
+        }
+
+        [Authorize(Roles = UserRolesUtility.OwnerAndSupervisor)]
+        public async Task<IActionResult> EmployeeForm(int idEmpployee) 
+        {
+            if (idEmpployee > 0)
+            {
+                HttpJwtUtility jwtUtils = new HttpJwtUtility(_config, HttpContext);
+                if (string.IsNullOrEmpty(jwtUtils.GetJwtCookie()))
+                {
+                    TempData["ActionFailed"] = "Jwt is broken. Please logout and then login again!";
+                    return RedirectToAction("index", "home");
+                }
+                string url = string.Format(_employeeDetailsUrl, idEmpployee);
+                var response = await HttpRequestUtility.SendSecureRequestJwtAsync(url, Utils.HttpMethods.GET, null, jwtUtils.GetJwtCookie());
+                if (response == null)
+                {
+                    TempData["ActionFailed"] = "Unable connect to server the external server. You can't make a new reservation, please try again later.";
+                    return RedirectToAction("index", "home");
+                }
+
+                var contentResponse = await response.Content.ReadAsStringAsync();
+                var employee = JsonConvert.DeserializeObject<EmployeeModel>(contentResponse);
+                return View(employee);
+            }
+
+            return View();
         }
 
         public IActionResult MyRestaurant()
