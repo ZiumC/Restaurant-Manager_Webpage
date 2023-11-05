f6c488826af9c10ca840db66d10483a63976bd90
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-11-05 18:17:16 +0100

    adding or updating employee works great

---
+++ Restaurants_Webpage/Restaurants_Webpage/Utils/Validator/EmployeeValidator.cs
@@ -0,0 +1,62 @@
+﻿using Restaurants_Webpage.Models.UserModels.EmployeeModels;
+
+namespace Restaurants_Webpage.Utils.Validator
+{
+    public class EmployeeValidator
+    {
+        public static bool IsDefectedEmployee(EmployeeModel employeeModel, IConfiguration config) 
+        {
+            if (string.IsNullOrEmpty(employeeModel.FirstName)) 
+            {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(employeeModel.LastName))
+            {
+                return true;
+            }
+
+            var pesel = employeeModel.PESEL;
+            if (string.IsNullOrEmpty(pesel))
+            {
+                return true;
+            }
+
+            if (pesel.Count() < 0 || pesel.Count() > 11) 
+            {
+                return true;
+            }
+
+            if (employeeModel.Salary < 0)
+            {
+                return true;
+            }
+
+            decimal result;
+            if (decimal.TryParse(config["ApplicationSettings:BasicBonus"], out result)) 
+            {
+                if (employeeModel.BonusSalary < result)
+                {
+                    return true;
+                }
+            }
+
+            if (string.IsNullOrEmpty(employeeModel.Address.City))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(employeeModel.Address.Street))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(employeeModel.Address.BuildingNumber))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
