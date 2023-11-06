0166f292e6230cdac681c98a3b700d60dc7343a3
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-11-06 16:21:42 +0100

    code has been refacotred

--- Restaurants_Webpage/Restaurants_Webpage/Utils/HttpJwtUtility.cs
+++ Restaurants_Webpage/Restaurants_Webpage/Utils/HttpJwtUtility.cs
@@ -2,9 +2,14 @@
 
 namespace Restaurants_Webpage.Utils
 {
+    public enum JwtFields { NAME, ROLE, EMP_ID, CLIENT_ID }
     public class HttpJwtUtility
     {
         private readonly string? _jwtCookie;
+        private readonly string _jwtCookieNameField;
+        private readonly string _jwtCookieRoleField;
+        private readonly string _jwtCookieEmpIdField;
+        private readonly string _jwtCookieClientIdField;
         private readonly IConfiguration _config;
 
         public HttpJwtUtility(IConfiguration config, HttpContext httpContext)
@@ -13,6 +18,11 @@ namespace Restaurants_Webpage.Utils
 
             string jwtCookieName = _config["ApplicationSettings:JwtSettings:CookieSettings:CookieName"];
 
+            string jwtCookieNameField = _config["ApplicationSettings:UserSettings:CookieSettings:Common:FieldName"];
+            string jwtCookieRoleField = _config["ApplicationSettings:UserSettings:CookieSettings:Common:RoleName"];
+            string jwtCookieClientIdField = _config["ApplicationSettings:UserSettings:CookieSettings:Client:IdName"];
+            string jwtCookieEmpIdField = _config["ApplicationSettings:UserSettings:CookieSettings:Supervisor:IdName"];
+
             try
             {
                 if (string.IsNullOrEmpty(jwtCookieName))
@@ -20,7 +30,32 @@ namespace Restaurants_Webpage.Utils
                     throw new Exception("Jwt cookie namne can't be empty");
                 }
 
+                if (string.IsNullOrEmpty(jwtCookieNameField))
+                {
+                    throw new Exception("Jwt cookie name field can't be empty");
+                }
+
+                if (string.IsNullOrEmpty(jwtCookieRoleField))
+                {
+                    throw new Exception("Jwt cookie role name can't be empty");
+                }
+
+                if (string.IsNullOrEmpty(jwtCookieClientIdField))
+                {
+                    throw new Exception("Jwt cookie ClientId name can't be empty");
+                }
+
+                if (string.IsNullOrEmpty(jwtCookieEmpIdField))
+                {
+                    throw new Exception("Jwt cookie EmpId name can't be empty");
+                }
+
                 _jwtCookie = httpContext.Request.Cookies[jwtCookieName];
+
+                _jwtCookieNameField = jwtCookieNameField;
+                _jwtCookieRoleField = jwtCookieRoleField;
+                _jwtCookieClientIdField = jwtCookieClientIdField;
+                _jwtCookieEmpIdField = jwtCookieEmpIdField;
             }
             catch (Exception ex)
             {
@@ -38,6 +73,43 @@ namespace Restaurants_Webpage.Utils
             if (_jwtCookie != null)
             {
                 var tokenContent = new JwtSecurityTokenHandler().ReadToken(_jwtCookie) as JwtSecurityToken;
+                return tokenContent?.Claims.First(claim => claim.Type == fieldName).Value;
+            }
+
+            return null;
+        }
+
+        public string? GetJwtCookieValue(JwtFields jwtCookieField)
+        {
+            if (_jwtCookie != null)
+            {
+
+                string fieldName = "";
+
+                switch (jwtCookieField) 
+                {
+                    case JwtFields.NAME:
+                        fieldName = _jwtCookieNameField;
+                        break;
+
+                    case JwtFields.ROLE:
+                        fieldName = _jwtCookieRoleField;
+                        break;
+
+                    case JwtFields.EMP_ID:
+                        fieldName = _jwtCookieEmpIdField;
+                        break;
+
+                    case JwtFields.CLIENT_ID:
+                        fieldName = _jwtCookieClientIdField;
+                        break;
+
+                    default: 
+                        return null;
+                }
+
+                var tokenContent = new JwtSecurityTokenHandler().ReadToken(_jwtCookie) as JwtSecurityToken;
+
                 return tokenContent?.Claims.First(claim => claim.Type == fieldName).Value;
             }
 
