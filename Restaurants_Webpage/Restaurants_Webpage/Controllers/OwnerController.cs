c5112e41c0cf8ce4214f58c4f6cae71026821676
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-11-26 19:16:34 +0100

    added dialog that checks if you want to proceed

---
+++ Restaurants_Webpage/Restaurants_Webpage/Controllers/OwnerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants_Webpage.Controllers
+{
+    public class OwnerController : Controller
+    {
+        public IActionResult Complaints()
+        {
+            return View();
+        }
+    }
+}
--- Restaurants_Webpage/Restaurants_Webpage/wwwroot/js/site.js
+++ Restaurants_Webpage/Restaurants_Webpage/wwwroot/js/site.js
@@ -3,3 +3,35 @@
         $(this).remove();
     });
 }, 7000);
+
+
+let isCanceled = false;
+const dialog = document.querySelector("dialog");
+const noButton = document.getElementById("n-btn");
+const yesButton = document.getElementById("y-btn");
+function showModal(e) {
+    let formToSubmitId = ""
+    let message = "Do you want to";
+    let id = e.id;
+
+    if (id == "rm-from-rest") {
+        message = message + " remove employee from restaurant?"
+        formToSubmitId = "rm-emp-rest"
+    } else if (id = "rm-emp-data") {
+        message = message + " fire employee? This action can't be undone!"
+        formToSubmitId = "fire-emp-rest"
+    } else {
+
+    }
+
+    document.getElementById("message-dialog").innerHTML = message;
+
+    dialog.showModal();
+    yesButton.addEventListener("click", () => {
+        document.getElementById(formToSubmitId).submit();
+    });
+
+    noButton.addEventListener("click", () => {
+        dialog.close();
+    });
+}
