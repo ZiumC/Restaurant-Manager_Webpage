5b547873ec798afb97c673ee0ef6b750dca65667
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-11-28 20:34:01 +0100

    reformated view of employment, also tuned up code

---
+++ Restaurants_Webpage/Restaurants_Webpage/Models/UserModels/AdministrativeModels/BasicModels/BasicEmployeeTypesModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Restaurants_Webpage.Models.UserModels.AdministrativeModels.BasicModels
+{
+    public class BasicEmployeeTypesModel
+    {
+        public int IdType { get; set; }
+        public string Name { get; set; }
+    }
+}
