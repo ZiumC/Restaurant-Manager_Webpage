9d5db7fe7554b52388e321c1794d0efcb398c159
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-12-01 13:02:38 +0100

    feature that allows to assign employee to the restaurant works great. Also fixed bug with empty form of employment

--- Restaurants_Webpage/Restaurants_Webpage/Controllers/OwnerController.cs
+++ Restaurants_Webpage/Restaurants_Webpage/Controllers/OwnerController.cs
@@ -15,6 +15,7 @@ namespace Restaurants_Webpage.Controllers
         private readonly string _employeeDataUrl;
         private readonly string _employeeTypesUrl;
         private readonly string _removeEmpFromRestaurantUrl;
+        private readonly string _assignEmployeeToRestaurantUrl;
 
         public OwnerController(IConfiguration config)
         {
@@ -26,6 +27,7 @@ namespace Restaurants_Webpage.Controllers
             string removeEmpFromRestaurantUrl = restaurantBaseUrl + "/{0}" + _config["Endpoints:Paths:Employee"] + "/{1}";
             string employeeDataUrl = employeeBaseUrl + "/{0}";
             string employeeTypesUrl = restaurantBaseUrl + _config["Endpoints:Paths:Employee"] + _config["Endpoints:Paths:Types"];
+            string assignEmployeeToRestaurantUrl = restaurantBaseUrl + "/{0}" + _config["Endpoints:Paths:Employee"] + "/{1}" + _config["Endpoints:Paths:Type"] + "/{2}";
 
             try
             {
@@ -54,6 +56,11 @@ namespace Restaurants_Webpage.Controllers
                     throw new Exception("Employee types url can't be empty");
                 }
 
+                if (string.IsNullOrEmpty(assignEmployeeToRestaurantUrl))
+                {
+                    throw new Exception("Url for assigning employee to restaurant can't be empty");
+                }
+
             }
             catch (Exception ex)
             {
@@ -64,6 +71,7 @@ namespace Restaurants_Webpage.Controllers
             _removeEmpFromRestaurantUrl = removeEmpFromRestaurantUrl;
             _employeeDataUrl = employeeDataUrl;
             _employeeTypesUrl = employeeTypesUrl;
+            _assignEmployeeToRestaurantUrl = assignEmployeeToRestaurantUrl;
 
         }
 
@@ -106,14 +114,47 @@ namespace Restaurants_Webpage.Controllers
                 TempData["ActionFailed"] = await HttpRequestUtility.GetResponseMessage(response);
             }
 
-            return RedirectToAction("employees", "supervisor", new { idRestaurant });
+            return RedirectToAction("employment", "owner", new { idEmployee });
         }
 
         [Authorize(Roles = UserRolesUtility.Owner)]
-        public async Task<IActionResult> AssignEmployeeToRestaurant(int idEmployee, int idRestaurant, int idType) 
+        public async Task<IActionResult> AssignEmployeeToRestaurant(int idEmployee, int idRestaurant, int idType)
         {
 
-            return RedirectToAction("restaurants", "restaurant");
+            if (idEmployee <= 0 || idRestaurant <= 0 || idType <= 0)
+            {
+                TempData["ActionFailed"] = "Did you modified request?";
+                return RedirectToAction("restaurants", "restaurant");
+            }
+
+            HttpJwtUtility jwtUtils = new HttpJwtUtility(_config, HttpContext);
+            if (string.IsNullOrEmpty(jwtUtils.GetJwtRequestCookie()))
+            {
+                TempData["ActionFailed"] = "Jwt is broken. Please logout and then login again!";
+                return RedirectToAction("restaurants", "restaurant");
+            }
+
+            string url = string.Format(_assignEmployeeToRestaurantUrl, idRestaurant, idEmployee, idType);
+            var response =
+                await HttpRequestUtility.SendSecureRequestJwtAsync(url, Utils.HttpMethods.POST, null, jwtUtils.GetJwtRequestCookie());
+            if (response == null)
+            {
+                TempData["ActionFailed"] = "Unable connect to server the external server, please try again later.";
+                return RedirectToAction("restaurants", "restaurant");
+            }
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["ActionSucceeded"] = $"Employee has been successfully assigned to the restaurant!";
+            }
+            else
+            {
+                TempData["ActionFailed"] = await HttpRequestUtility.GetResponseMessage(response);
+            }
+
+            return RedirectToAction("employment", "owner", new { idEmployee });
+
         }
 
         [Authorize(Roles = UserRolesUtility.Owner)]
